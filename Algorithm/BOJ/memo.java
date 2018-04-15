ae82308aa956c0395f30fb90e6c01f10ffc211e0
habaak <joohn728@gmail.com>
2018-04-15 12:20:30 +0900

    ADD algorithm study

---
+++ Algorithm/BOJ/memo.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class memo {
+	static int N,M;
+	static int[] mat = new int[8];
+	static boolean[] visited = new boolean[8];
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+	}
+
+}
