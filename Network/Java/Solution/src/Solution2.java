2dda078058fb018d779b36c8373a53fb0c701436
habaak <joohn728@gmail.com>
2018-07-30 23:53:45 +0900

    ADD BOJ 11052

--- Network/Java/Solution/src/Solution2.java
+++ Network/Java/Solution/src/Solution2.java
@@ -1,52 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
-class Solution2 {
-    public static int min = 9;
-    public static int[] arr;
-    public static Stack<Integer> stack = new Stack<>();
-    
-    public int solution(int[] numbers) {
-        arr = numbers;
-        int n = numbers.length;
-        int m = 3;
-        int answer = 0;
-        dfs(0, n, m, new boolean[n + 1]);
-        return answer;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+ 
+        int N=input.nextInt();
+ 
+        int P[]=new int[N+1];
+ 
+        for(int i=1; i<=N; i++)
+        {
+            P[i]=input.nextInt();
+        }
+ 
+        int dp[]=new int[N+1];        // n개 팔았을때 최대수익
+        dp[0]=0;                    
+        dp[1]=P[1];        
+ 
+        for(int i=2; i<=N; i++)
+        {
+            dp[i]=P[i];
+            for(int j=0; j<=i; j++)
+            {
+                dp[i]=Math.max(dp[i], dp[i-j]+dp[j]);
+            }
+        }
+ 
+        System.out.println(dp[N]);
     }
-    
-    public static void dfs(int depth, int n, int m, boolean[] k) {
-
-      boolean[] vistied = new boolean[n + 1];
-
-      if (depth == m) {
-         int sum = 0;
-         int t = 1;
-         for (int num : stack) {
-            sum += num * t;
-            t *= 10;
-         }
-
-         if (sum < 100) {
-            return;
-           
-         double d = ((double) sum / 7 - sum / 7) * 10;
-         if (min > (int) d && (int) d != 0)
-            min = (int) d;
-
-         return;
-      }
-
-      for (int i = 1; i <= n; i++) {
-         if (!vistied[i] && !k[i]) {
-            stack.push(arr[i]);
-            k[i] = true;
-            dfs(depth + 1, n, m, k);
-            stack.pop();
-            k[i] = false;
-         }
-      }
-   }
 }
